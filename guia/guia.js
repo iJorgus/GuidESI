d2f8db2830ae89cc7e084fbd9aacaa01c42bbc35
JuanFdezGav <164228057+JuanFdezGav@users.noreply.github.com>
2026-03-05 19:06:02 +0100

    Update guia.js

--- guia/guia.js
+++ guia/guia.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnMapa = document.getElementById("btn-mapa");
+    const popup = document.getElementById("popup-container");
+    const btnCerrar = document.getElementById("cerrar-popup");
 
+    // Función para abrir el pop-up
+    btnMapa.addEventListener("click", () => {
+        popup.style.display = "flex"; // Cambiamos de 'none' a 'flex' para mostrarlo
+    });
+
+    // Función para cerrar el pop-up al pulsar la X
+    btnCerrar.addEventListener("click", () => {
+        popup.style.display = "none";
+    });
+
+    // EXTRA: Cerrar si el usuario hace clic fuera de la imagen (en el fondo oscuro)
+    popup.addEventListener("click", (e) => {
+        if (e.target === popup) {
+            popup.style.display = "none";
+        }
+    });
+});
