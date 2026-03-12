253bd9c6bd0626a1331f023060f8dce4b81e2ec1
Jorge Bolívar Blandino <163106579+iJorgus@users.noreply.github.com>
2026-03-12 10:13:32 +0100

    Update importar.js

--- importar.js
+++ importar.js
@@ -1,13 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Buscamos el contenedor donde irá el navbar
-    const contenedor = document.getElementById("navbar-container");
+    
+    // --- IMPORTAR NAVBAR ---
+    const contenedorNavbar = document.getElementById("navbar-container");
 
-    // Usamos fetch para "traer" el archivo navbar.html
     fetch("navbar.html")
         .then(response => response.text())
         .then(data => {
-            // Metemos el HTML del navbar dentro del contenedor
-            contenedor.innerHTML = data;
+            contenedorNavbar.innerHTML = data;
         })
         .catch(error => console.error("Error al cargar el navbar:", error));
+
+
+    // --- IMPORTAR FOOTER ---
+    const contenedorFooter = document.getElementById("footer-container");
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            contenedorFooter.innerHTML = data;
+        })
+        .catch(error => console.error("Error al cargar el footer:", error));
+
 });
